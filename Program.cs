76fe0ba554106d33683c69a23c8606042f241463
Nutelinha <matheus.rlima9@aluno.senai.br>
2022-08-25 11:05:07 -0300

    fouth commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using VoeAirlinesSenai.Contexts;
+using VoeAirlinesSenai.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<VoeAirlinesSenaiContexts>();
+builder.Services.AddTransient<AeronaveServices>();
 
 var app = builder.Build();
 
